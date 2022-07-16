4bf84624af68546409014153ba4d95303165ae68
haagmin5382 <itworker5382@gmail.com>
2022-07-16 23:39:50 +0900

    feat : add calculator function

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,10 +8,23 @@ import { SelectChangeEvent } from "@mui/material/Select";
 import { getRateData } from "../utils/index";
 
 const Main = () => {
-  const [currency1, setCurrency1] = useState("");
+  // 6 => 위안화 , 8 => 유로화 , 9 => 영국 파운드 , 12 => 일본 엔 ,13 => 한국 원 , 22 => 미국 달러
+  interface currencyDataType {
+    [key: string]: number;
+  }
+  const currencyData: currencyDataType = {
+    CHINA: 6,
+    EURO: 8,
+    UK: 9,
+    JAPAN: 12,
+    KOREA: 13,
+    USA: 22,
+  };
+
+  const [currency1, setCurrency1] = useState("USA");
   const [value1, setValue1] = useState("");
 
-  const [currency2, setCurrency2] = useState("");
+  const [currency2, setCurrency2] = useState("KOREA");
   const [value2, setValue2] = useState("");
 
   const handleCurrency1 = (event: SelectChangeEvent) => {
@@ -26,9 +39,30 @@ const Main = () => {
   const handleValue2 = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue2(event.target.value as string);
   };
+  console.log(
+    "currency1 : ",
+    currencyData[currency1],
+    "currency2 : ",
+    currencyData[currency2]
+  );
+
   useEffect(() => {
-    // setValue2((Number(value1) * 1000).toString());
-  }, [value1, value2]);
+    getRateData().then(({ data }) => {
+      const rate =
+        currencyData[currency1] === 12
+          ? Number(data[currencyData[currency1]].bkpr.replace(",", "")) / 100
+          : Number(data[currencyData[currency1]].bkpr.replace(",", ""));
+
+      const rate2 =
+        currencyData[currency2] === 12
+          ? Number(data[currencyData[currency2]].bkpr.replace(",", "")) / 100
+          : Number(data[currencyData[currency2]].bkpr.replace(",", ""));
+
+      console.log("rate : ", rate, ", rate2 : ", rate2);
+      setValue2(((Number(value1) * Number(rate)) / Number(rate2)).toString());
+    });
+  }, [currency1, currency2, value1, value2]);
+
   return (
     <div>
       <CssBaseline />
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 export function getRateData() {
-  return axios.get(`/api?authkey=${process.env.REACT_APP_KEY}&data=AP01`);
+  return axios.get(
+    `/api?authkey=${process.env.REACT_APP_KEY}&searchdate=${20220715}&data=AP01`
+  );
 }
