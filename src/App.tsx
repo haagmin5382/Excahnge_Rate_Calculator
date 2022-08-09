895b6ecdd6f895b350e067adb9ca77e5a9c2f4b6
haagmin5382 <itworker5382@gmail.com>
2022-08-09 20:35:55 +0900

    style : add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Main from "components/Main";
 import Header from "components/Header";
+import Footer from "components/Footer";
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Box
+          component="footer"
+          sx={{
+            py: 3,
+            px: 2,
+            mt: 3,
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[200]
+                : theme.palette.grey[800],
+          }}
+        >
+          <Container>
+            <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+              환율 정보 출처 : 한국 수출입 은행 환율 정보
+            </Typography>
+          </Container>
+        </Box>
+      </Box>
+    </footer>
+  );
+};
+
+export default Footer;
